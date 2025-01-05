da394061a0149521fec048a326d721c185d3ea0c
Joel Ramirez <joelr6509@gmail.com>
2025-01-05 09:57:56 -0600

    se midifico el footer de copyright

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
-              <span>Copyright © SOFTNIC 2024. Todos los Derechos Reservados.</span>
+              <span>Copyright © SOFTNIC 2025. Todos los Derechos Reservados.</span>
             </div>
           </div>
         </footer>
